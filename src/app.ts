840548b85b8a049331acfaf1cfe89ef094ae882f
a-fokin <a.fokin@sxope.com>
2025-04-14 15:12:43 +0300

    fixed conf

--- src/app.ts
+++ src/app.ts
@@ -1,58 +1,23 @@
 import express from 'express';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import cors from 'cors';
-import pool from './config/db';
-
-const app = express();
-
-pool.query('SELECT NOW()', (err,res) => {
-  if(err) {
-    console.error('Error connecting to the database', err.stack);
-  } else {
-    console.log('Connected to the database:', res.rows);
-  }
-})
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Routes
-app.get('/api/healthcheck', (req, res) => {
-    res.status(200).json({
-      status: 'success',
-      message: 'API is sdf update',
-      timestamp: new Date().toISOString(),
-      environment: process.env.NODE_ENV || 'development',
-      version: '1.0.0'
-    });
-  });
-
-// app.use('/api/v1', exampleRoutes);
-=======
-import { errorHandler } from './middlewares/errorHandler';
-=======
-import { NotFoundException } from './exceptions/http.exception';
+// import cors from 'cors';
 import { errorMiddleware } from './middlewares/error.middleware';
->>>>>>> 1ff89ae (added test route)
+import { NotFoundException } from './exceptions/http.exception';
 import itemRouter from './routes/itemRoutes';
 
-
 const app = express();
 
-app.use(express.json())
+
+// Middleware
+// app.use(cors());
+app.use(express.json());
 
 app.use('/api/items', itemRouter)
 
-<<<<<<< HEAD
-app.use(errorHandler)
->>>>>>> 4be350b (added express)
-=======
+app.use(errorMiddleware)
+
 app.use((req, res, next) => {
     next(new NotFoundException('Endpoint not found'));
   });
   app.use(errorMiddleware);
->>>>>>> 1ff89ae (added test route)
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
-import app from './app';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-=======
-
 import app from "./app";
 import config from "./config/config";
 
 
 app.listen(config.port, () => {
     console.log(`Server running on port ${config.port}`);
->>>>>>> 4be350b (added express)
 });
